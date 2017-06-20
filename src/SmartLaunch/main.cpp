c959321dac751a871108793116fc0e3b52a7a389
samson <zl.wang@qq.com>
2017-06-20 22:44:12 +0800

    update lisence declaration

--- src/SmartLaunch/main.cpp
+++ src/SmartLaunch/main.cpp
@@ -1,5 +1,5 @@
 /*
-SmartLaunch: multi-function launcher.
+SmartLaunch: multi-function app launcher.
 Copyright (C) 2017 Samson Wang
 
 This program is free software: you can redistribute it and/or modify
